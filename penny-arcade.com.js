d865f1f3ebe12a9d1928b761e4ea75c1b852eec1
Matthew Batchelder <borkweb@gmail.com>
2014-02-18 09:45:57 -0500

    Position the text icon in the circle

--- penny-arcade.com.js
+++ penny-arcade.com.js
@@ -5,6 +5,7 @@ dotjs_pa.init = function() {
 	$('#main').css( 'padding-top', '5px' );
 	$('#comic .navTop').hide();
 	$('.copy.comicTag h4 a').css( {
+		backgroundPosition: '50%',
 		backgroundSize: '75% 75%',
 		height: '25px',
 		width: '25px'
