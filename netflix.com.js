0438cdaeac728f108d205a035cc30706d1c13faa
Matthew Batchelder <borkweb@gmail.com>
2014-03-06 15:02:17 -0500

    Add comment

--- netflix.com.js
+++ netflix.com.js
@@ -1,3 +1,5 @@
+// this unbreaks the console.log breaking on netflix.
+// Props goes to zbtirrell who found the configurable:false which led us to writable:false
 var s = document.createElement('script');
-s.innerHTML = 'Object.defineProperty(window, "console", {configurable: false,writable: false});';
+s.innerHTML = 'Object.defineProperty(window, "console", {writable: false});';
 document.head.appendChild( s );
